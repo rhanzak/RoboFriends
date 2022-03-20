0f94971444ad03380800de7061ff08db90a024b8
Ray Hanzak <rhanzak10@gmail.com>
2022-03-20 16:09:55 -0400

    add search functionality and components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
+import SearchBox from "./SearchBox";
+
 class App extends Component {
-    render(){
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+
+    onSearchChange = (event) => { 
+        this.setState({ searchField: event.target.value });
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+        })
+
         return (
             <div className="tc">
                 <h1>RoboFriends</h1>
-                <CardList robots={robots} />
+                <SearchBox searchChange={ this.onSearchChange }/>
+                <CardList robots={filteredRobots} />
             </div>
         );
     }
