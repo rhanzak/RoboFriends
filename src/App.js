8a47be65c287cbcf938d1146c15d30da385b2ace
Ray Hanzak <rhanzak10@gmail.com>
2022-03-20 15:55:38 -0400

    add cards and cardlist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from "react";
+import CardList from "./CardList";
+import { robots } from "./robots";
+class App extends Component {
+    render(){
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <CardList robots={robots} />
+            </div>
+        );
+    }
 }
 
 export default App;
