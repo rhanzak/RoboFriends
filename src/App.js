3aad93f8663822ff60b7eec1f79d226cc8fcd694
Ray Hanzak <rhanzak10@gmail.com>
2022-03-20 17:56:55 -0400

    Children component scrolling feature

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import "./App.css"
+import "./App.css";
+import Scroll from "./Scroll.js";
 
 class App extends Component {
     constructor() {
@@ -37,8 +38,10 @@ class App extends Component {
             return (
                 <div className="tc">
                     <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={ this.onSearchChange }/>
-                    <CardList robots={filteredRobots} />
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filteredRobots} />
+                    </Scroll>
                 </div>
             );
         }
