8826934fafdfeaa34d245cccbe938fc8925e4bc1
Ray Hanzak <ray@outerbox.com>
2022-03-21 21:16:26 -0400

    comment out count example

--- src/containers/App.js
+++ src/containers/App.js
@@ -9,7 +9,7 @@ function App() {
     const [robots, setRobots] = useState([])
     const [searchField, setSearchField] = useState('')
     
-    const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
 
     const onSearchChange = (event) => { 
         setSearchField( event.target.value );
@@ -22,8 +22,8 @@ function App() {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
             .then(users => { setRobots(users) })
-        console.log(count);
-    },[count]) // only run if count changes
+        // console.log(count);
+    },[]) // only run if count changes
         
     const filteredRobots = robots.filter(robot => {
         return robot.name.toLowerCase().includes(searchField.toLowerCase())
@@ -36,7 +36,7 @@ function App() {
         : 
         <div className="tc">
             <h1 className="f1">RoboFriends</h1>
-            <button onClick={() => setCount(count+1)}>Click Me!</button>
+            {/* <button onClick={() => setCount(count+1)}>Click Me!</button> */}
             <SearchBox searchChange={onSearchChange} />
             <Scroll>
                 <CardList robots={filteredRobots} />
